c768a8a8cb270895f2ed371d748a3418a33c8fbd
Kashish980 <59578312+Kashish980@users.noreply.github.com>
2020-10-03 22:36:48 +0530

    Add files via upload

---
+++ Addcommandline.java
@@ -0,0 +1,10 @@
+class 	Addcommandline
+{
+   public static void main(String args[])
+   {
+    double a = Double.parseDouble(args[0]);
+    double b = Double.parseDouble(args[1]);
+    double c= a+b;
+	System.out.println("kashish Srivatava 079 \n sum of a and b is:" + c);
+	}
+}

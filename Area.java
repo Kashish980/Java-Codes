9d797bc81f93186634213f8aebe8dc58e8d1fb78
Kashish Srivastava <59578312+Kashish980@users.noreply.github.com>
2020-11-09 10:26:44 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,3 +1,5 @@
+//This is the program to find out the area and circumference of a circle where radius will be provided by the user.
+
 import java.util.*;
 import java.lang.*; //default package
 class 	Area
@@ -6,12 +8,12 @@ class 	Area
    {
    Scanner s=new Scanner(System.in);
    double rar,cir,a;
-    System.out.println("Enter radius no.");
-	rar=s.nextDouble();
-	a=Math.PI*Math.pow(rar,2);
+        System.out.println("Enter radius no.");
+	rar=s.nextDouble();  //user has to enter the radius
+	a=Math.PI*Math.pow(rar,2);  // area formula impplementation = pie*radius*radius
 	cir=2*Math.PI*rar;
 	System.out.println("Area is:" + a);
-		System.out.println("Circumference is:" + cir);
+	System.out.println("Circumference is:" + cir);
 
 	}
 }
